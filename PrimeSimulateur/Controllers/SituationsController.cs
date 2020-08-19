658e2ee8ff7782b0bfa0a187bcad18c326dcf7af
sofreshmail1 <sofreshmail1@gmail.com>
2020-08-19 17:30:11 +0100

    My first commit

---
+++ PrimeSimulateur/Controllers/SituationsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrimeSimulateur.Models;
+
+namespace PrimeSimulateur.Controllers
+{
+    [Authorize]
+    public class SituationsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public SituationsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Situations
+        public async Task<IActionResult> Index()
+        {
+            var myDbContext = _context.Situations.Include(s => s.Client);
+            return View(await myDbContext.ToListAsync());
+        }
+
+        // GET: Situations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var situation = await _context.Situations
+                .Include(s => s.Client)
+                .FirstOrDefaultAsync(m => m.SituationId == id);
+            if (situation == null)
+            {
+                return NotFound();
+            }
+
+            return View(situation);
+        }
+
+        // GET: Situations/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "email");
+            return View();
+        }
+
+        // POST: Situations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SituationId,Nombredepersonne,Revenumenage,ClientId")] Situation situation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(situation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "email", situation.ClientId);
+            return View(situation);
+        }
+
+        // GET: Situations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var situation = await _context.Situations.FindAsync(id);
+            if (situation == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "email", situation.ClientId);
+            return View(situation);
+        }
+
+        // POST: Situations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SituationId,Nombredepersonne,Revenumenage,ClientId")] Situation situation)
+        {
+            if (id != situation.SituationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(situation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SituationExists(situation.SituationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "email", situation.ClientId);
+            return View(situation);
+        }
+
+        // GET: Situations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var situation = await _context.Situations
+                .Include(s => s.Client)
+                .FirstOrDefaultAsync(m => m.SituationId == id);
+            if (situation == null)
+            {
+                return NotFound();
+            }
+
+            return View(situation);
+        }
+
+        // POST: Situations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var situation = await _context.Situations.FindAsync(id);
+            _context.Situations.Remove(situation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SituationExists(int id)
+        {
+            return _context.Situations.Any(e => e.SituationId == id);
+        }
+    }
+}
---
+++ PrimeSimulateur/Models/trace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeSimulateur.Models
+{
+    public class trace
+    {
+        [Key]
+        [Required]
+        public int traceId { get; set; }
+
+       
+        [Required]
+
+        public int ClientId{ get; set; }
+        public Client Client { get; set; }
+        public string Type{ get; set; }
+        public string Nom{ get; set; }
+        public float Surface{ get; set; }
+        public float prime { get; set; }
+
+
+
+    }
+}
